f467c5d3510b7e4d764f7287b32efe910057cde2
ShaxnozaNizomova <xurshidatosheva1918@gmail.com>
2023-10-19 10:52:55 +0500

    homework

---
+++ main.js
@@ -0,0 +1,31 @@
+//1-masala
+ /*function Names(...names){
+    let arr1 = ["John","Sarah","Mike","Alex"]
+    for (i = 0; i < arr1.length; i++){
+        if(arr1[i]==names){
+            console.log(` ${names} bor`);
+        }else{
+            console.log(` ${names} yoq`);
+        }
+    }
+}Names("jack") */
+
+// 2-masala
+
+/*numbers = [0, 2, 4, 6, 8, 10, 12]
+let largest = numbers[0];
+let smallest = numbers[0];
+for (let i = 1; i < numbers.length; i++) {
+  if (numbers[i] > largest) {
+    largest = numbers[i];
+  } else if (numbers[i] < smallest) {
+    smallest = numbers[i];
+  }}
+  console.log(largest);
+  console.log(smallest);*/
+
+// 3-masala
+
+let arr1 = [0 ,1, 2, 3, 4, 5, 6]
+let arr2 = []
+if()
